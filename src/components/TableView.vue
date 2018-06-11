7bc7f1bae442c19257f58e0331973100e854ce3c
Max Mayr <max.mayr@gmail.com>
2018-06-11 14:32:45 +0200

    added table view and graphql stuff

--- src/components/TableView.vue
+++ src/components/TableView.vue
@@ -4,38 +4,28 @@
                 :data="data"
                 :loading="loading"
 
-                paginated
-                backend-pagination
-                :total="total"
-                :per-page="perPage"
-                @page-change="onPageChange"
-
-                backend-sorting
                 :default-sort-direction="defaultSortOrder"
-                :default-sort="[sortField, sortOrder]"
-                @sort="onSort">
+                :default-sort="[sortField, sortOrder]">
 
             <template slot-scope="props">
-                <b-table-column field="original_title" label="Title" sortable>
-                    {{ props.row.original_title }}
+                <b-table-column field="symbol" label="Symbol" sortable>
+                    {{ props.row.symbol }}
                 </b-table-column>
 
-                <b-table-column field="vote_average" label="Vote Average" numeric sortable>
-                    <span class="tag" :class="type(props.row.vote_average)">
-                        {{ props.row.vote_average }}
-                    </span>
+                <b-table-column field="name" label="Name" sortable>
+                    {{ props.row.name }}
                 </b-table-column>
 
-                <b-table-column field="vote_count" label="Vote Count" numeric sortable>
-                    {{ props.row.vote_count }}
+                <b-table-column field="priceUSD" label="priceUSD" numeric sortable>
+                    {{ props.row.priceUSD }}
                 </b-table-column>
 
-                <b-table-column field="release_date" label="Release Date" sortable centered>
-                    {{ props.row.release_date ? new Date(props.row.release_date).toLocaleDateString() : '' }}
+                <b-table-column field="priceBTC" label="priceBTC" numeric sortable>
+                    {{ props.row.priceBTC }}
                 </b-table-column>
 
-                <b-table-column label="Overview" width="500">
-                    {{ props.row.overview | truncate(80) }}
+                <b-table-column field="marketCapUSD" label="marketCapUSD" numeric sortable>
+                    {{ props.row.marketCapUSD }}
                 </b-table-column>
             </template>
         </b-table>
@@ -43,14 +33,55 @@
 </template>
 
 <script>
+    import gql from 'graphql-tag';
+
     export default {
         name: 'TableView',
+        apollo: {
+            $loadingKey: 'loading',
+            tickers() {
+                return {
+                    query: gql`query {
+                         tickers {
+                             name
+                             symbol
+                             rank
+                             priceUSD
+                             priceBTC
+                             dailyVolumeUSD
+                             marketCapUSD
+                             availableSupply
+                             totalSupply
+                             percentChange1h
+                             percentChange24h
+                             percentChange7d
+                             lastUpdated
+                         }
+                    }`,
+                    fetchPolicy: 'cache-and-network',
+                    result(result) {
+                        console.log(result);
+                        this.data = []
+                        if (result.data) {
+                            let currentTotal = result.data.tickers.length
+                            if (currentTotal / this.perPage > 100) {
+                                currentTotal = this.perPage * 100
+                            }
+                            this.total = currentTotal;
+                            result.data.tickers.forEach((item) => {
+                                this.data.push(item)
+                            })
+                        }
+                    },
+                }
+            },
+        },
         data() {
             return {
                 data: [],
                 total: 0,
                 loading: false,
-                sortField: 'vote_count',
+                sortField: 'marketCapUSD',
                 sortOrder: 'desc',
                 defaultSortOrder: 'desc',
                 page: 1,
@@ -59,83 +90,33 @@
         },
         methods: {
             /*
-             * Load async data
-             */
-            loadAsyncData() {
-                const params = [
-                    'api_key=bb6f51bef07465653c3e553d6ab161a8',
-                    'language=en-US',
-                    'include_adult=false',
-                    'include_video=false',
-                    `sort_by=${this.sortField}.${this.sortOrder}`,
-                    `page=${this.page}`
-                ].join('&')
-
-                this.loading = true
-                this.$http.get(`https://api.themoviedb.org/3/discover/movie?${params}`)
-                    .then(({data}) => {
-                        // api.themoviedb.org manage max 1000 pages
-                        console.log(data);
-                        this.data = []
-                        let currentTotal = data.total_results
-                        if (data.total_results / this.perPage > 1000) {
-                            currentTotal = this.perPage * 1000
-                        }
-                        this.total = currentTotal
-                        data.results.forEach((item) => {
-                            item.release_date = item.release_date.replace(/-/g, '/')
-                            this.data.push(item)
-                        })
-                        this.loading = false
-                    })
-                    .catch((error) => {
-                        this.data = []
-                        this.total = 0
-                        this.loading = false
-                        throw error
-                    })
-            },
-            /*
              * Handle page-change event
              */
-            onPageChange(page) {
-                this.page = page
-                this.loadAsyncData()
-            },
-            /*
-             * Handle sort event
-             */
-            onSort(field, order) {
-                this.sortField = field;
-                this.sortOrder = order;
-                this.loadAsyncData()
-            },
-            /*
-             * Type style in relation to the value
-             */
-            type(value) {
-                const number = parseFloat(value)
-                if (number < 6) {
-                    return 'is-danger'
-                } else if (number >= 6 && number < 8) {
-                    return 'is-warning'
-                } else if (number >= 8) {
-                    return 'is-success'
-                }
-            }
-        },
-        filters: {
-            /**
-             * Filter to truncate string, accepts a length parameter
-             */
-            truncate(value, length) {
-                return value.length > length
-                    ? value.substr(0, length) + '...'
-                    : value
-            }
-        },
-        mounted() {
-            this.loadAsyncData()
+            // onPageChange(page) {
+            //     this.page = page
+            //     this.loadAsyncData()
+            // },
+            // /*
+            //  * Handle sort event
+            //  */
+            // onSort(field, order) {
+            //     this.sortField = field;
+            //     this.sortOrder = order;
+            //     this.loadAsyncData()
+            // },
+            // /*
+            //  * Type style in relation to the value
+            //  */
+            // type(value) {
+            //     const number = parseFloat(value)
+            //     if (number < 6) {
+            //         return 'is-danger'
+            //     } else if (number >= 6 && number < 8) {
+            //         return 'is-warning'
+            //     } else if (number >= 8) {
+            //         return 'is-success'
+            //     }
+            // }
         }
     }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,35 @@ import App from './App.vue'
 import Buefy from 'buefy'
 import 'buefy/lib/buefy.css'
 import VueResource from 'vue-resource';
+import {ApolloClient} from 'apollo-client'
+import {HttpLink} from 'apollo-link-http'
+import {InMemoryCache} from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
 
+const httpLink = new HttpLink({
+    // You should use an absolute URL here
+    uri: 'http://localhost:3000/graphql',
+});
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+    connectToDevTools: true,
+});
+
+const apolloProvider = new VueApollo({
+    defaultClient: apolloClient,
+});
+
+// Install the vue plugin
+Vue.use(VueApollo);
 Vue.use(VueResource);
+Vue.use(Buefy);
 
-Vue.use(Buefy)
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
 
 new Vue({
+    provide: apolloProvider.provide(),
     render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
